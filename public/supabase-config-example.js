35dba939e95c937c1a6183a8959fea9a107a38df
Mahd-Shakil <mahdshakil@outlook.com>
2026-07-27 13:57:16 -0400

    Update supabase config file

--- public/supabase-config-example.js
+++
@@ -1,5 +0,0 @@
-
-const SUPABASE_URL = 
-const SUPABASE_ANON_KEY = 
-
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
---
+++ public/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://vqiuowihefogxlnjxjjr.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxaXVvd2loZWZvZ3hsbmp4ampyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwOTgzMzAsImV4cCI6MjEwMDY3NDMzMH0.IuvKWFzKBD7aG9sX3cef48pe8gm8y43vOIFbAFxlzyI';
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
